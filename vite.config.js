7b4c560a101048e3fcf9fd5e75c30d700bbe7de9
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2025-03-24 21:33:24 -0300

    Ultima version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Inventario/', 
   plugins: [react()],
-})
+  build: {
+    outDir: 'docs', 
+  },
+  base: './', 
+});
